1207007605cf166dfeaaced3192d9bd4ac3f3b92
mortalhowl <mortalhowl@gmail.com>
2026-08-14 11:20:08 +0700

    chore: config dayjs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dayjs from "@/lib/dayjs";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString: string): string {
+  return dayjs(dateString).format("DD/MM/YYYY");
+}
+
+export function formatDateTime(dateString: string): string {
+  return dayjs(dateString).format("DD/MM/YYYY HH:mm");
+}
+
+export function fromNow(dateString: string): string {
+  return dayjs(dateString).fromNow();
 }
